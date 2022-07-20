f1999d2c863b3d316a9ce05d7f384ca69d64fb1d
adotland <1982042+adotland@users.noreply.github.com>
2022-07-19 19:27:14 -0700

    build updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import Prism from 'prism-react-renderer/prism'
-(typeof global !== "undefined" ? global : window).Prism = Prism
+// import Prism from 'prism-react-renderer/prism'
+// (typeof global !== "undefined" ? global : window).Prism = Prism
 // require("prismjs/components/prism-sql")
 // require("prismjs/components/prism-javascript")
 import { useEffect } from 'react'
